ba42228ad3e5309b28ab53ad0990a78333f9847e
Srbaqua <23bcs101@nith.ac.in>
2025-05-18 14:25:52 +0530

    Input added with HTML

--- Scripts/todolist.js
+++ Scripts/todolist.js
@@ -1,4 +1,13 @@
-const todoList = []
+const TodoList = [];
 function addTodo(){
-        
+const inputElement = document.querySelector('.todo');
+TodoList.push(inputElement.value);
+inputElement.value = '';
+
+var taskList = '';
+for(let i = 0;i<TodoList.length;i++){
+    taskList+=`<p>${TodoList[i]}<p>`;
+}
+document.querySelector('.show-Task').innerHTML = taskList;
+
 }
